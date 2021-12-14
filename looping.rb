77286e4e6934bccec617f39c179374d99c74aea6
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-12-14 12:31:31 -0500

    commit tree e362544e24b58d5710b3a55694094f39b085cf4a

--- looping.rb
+++ looping.rb
@@ -1,6 +1,17 @@
 def happy_new_year
-  # your code here
+  counter = 10
+
+  while counter > 0 do 
+    counter -=1
+    puts counter
+  end
+
+  puts "Happy New Year!"
 end
+
+happy_new_year
+
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -16,7 +27,11 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  # counter = 0
+  # while counter <100 do 
+  #   puts 'Fizz'
+  #   counter +=1
+  # end
 end
 
 def reverse_string(str)
